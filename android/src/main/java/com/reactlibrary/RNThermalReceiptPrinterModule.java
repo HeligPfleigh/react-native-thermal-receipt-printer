98d5a13106d404be0ee5f47baf770355cac1abbd
GoldenHands92 <tuan.tran@mttjsc.com>
2019-05-29 11:24:10 +0700

    missing import module

--- android/src/main/java/com/reactlibrary/RNThermalReceiptPrinterModule.java
+++ android/src/main/java/com/reactlibrary/RNThermalReceiptPrinterModule.java
@@ -5,6 +5,10 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class RNThermalReceiptPrinterModule extends ReactContextBaseJavaModule {
 
