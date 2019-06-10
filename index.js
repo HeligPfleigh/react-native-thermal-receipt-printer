dfbd27afb933d3a0266f62ea07a51661addf25f8
GoldenHands92 <tuan.tran@mttjsc.com>
2019-06-10 17:30:26 +0700

    Init function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { NativeModules } from 'react-native';
 
 const { RNThermalReceiptPrinter, RNBLEPrinter } = NativeModules;
 
-export const RNBLEPrinter = RNBLEPrinter;
-
-export default RNThermalReceiptPrinter;
+export {
+    RNThermalReceiptPrinter,
+    RNBLEPrinter
+}
--- ios/RNBLEPrinter.m
+++ ios/RNBLEPrinter.m
@@ -16,17 +16,48 @@
 {
     return dispatch_get_main_queue();
 }
+NSMutableArray* _printerArray;
+
 RCT_EXPORT_MODULE()
 
-RCT_REMAP_METHOD(testBLEFunction,
-                  resolver: (RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(init,
+                 init_resolver:(RCTPromiseResolveBlock)resolve
+                 init_rejecter:(RCTPromiseRejectBlock)reject) {
+    resolve(@"Init successful");
+}
+
+RCT_REMAP_METHOD(getDeviceList,
+                 get_device_list_resolver:(RCTPromiseResolveBlock)resolve
+                 get_device_list_rejecter:(RCTPromiseRejectBlock)reject) {
+    _printerArray = [NSMutableArray new];
     
+    [[PrinterSDK defaultPrinterSDK] scanPrintersWithCompletion:^(Printer* printer){
+        [_printerArray addObject:printer];
+    }];
     
-    NSString *greeting = @"it's just a test";
+    resolve(@[_printerArray]);
     
-    resolve(@[greeting]);
-    
+}
+
+RCT_EXPORT_METHOD(connectPrinter:(NSString *)innerAddress
+                  connect_printer_resolver:(RCTPromiseResolveBlock)resolve
+                  connect_printer_rejecter:(RCTPromiseRejectBlock)reject) {
+    // TODO
+    resolve(@"TODO");
+}
+
+RCT_EXPORT_METHOD(printRawData:(NSString *)base64
+                  print_data_resolver:(RCTPromiseResolveBlock)resolve
+                  print_data_rejecter:(RCTPromiseRejectBlock)reject) {
+    // TODO
+    resolve(@"TODO");
+}
+
+RCT_REMAP_METHOD(closeConn,
+                 close_connect_resolver:(RCTPromiseResolveBlock)resolve
+                 close_connect_rejecter:(RCTPromiseRejectBlock)reject) {
+    // TODO
+    resolve(@"TODO");
 }
 
 @end
