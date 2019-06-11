291a61ba9078197c0e2644fac8eab82ef7f2e37d
GoldenHands92 <tuan.tran@mttjsc.com>
2019-06-11 14:08:41 +0700

    Add missing code

--- ios/RNBLEPrinter.m
+++ ios/RNBLEPrinter.m
@@ -16,39 +16,45 @@
 {
     return dispatch_get_main_queue();
 }
-NSMutableArray* _printerArray;
 
 RCT_EXPORT_MODULE()
 
 RCT_REMAP_METHOD(init,
                  init_resolver:(RCTPromiseResolveBlock)resolve
                  init_rejecter:(RCTPromiseRejectBlock)reject) {
+    _printerArray = [NSMutableArray new];
     resolve(@"Init successful");
 }
 
 RCT_REMAP_METHOD(getDeviceList,
                  get_device_list_resolver:(RCTPromiseResolveBlock)resolve
                  get_device_list_rejecter:(RCTPromiseRejectBlock)reject) {
-    _printerArray = [NSMutableArray new];
-    
-    [[PrinterSDK defaultPrinterSDK] scanPrintersWithCompletion:^(Printer* printer){
-        [_printerArray addObject:printer];
-    }];
-    
-    resolve(@[_printerArray]);
-    
+    if (!_printerArray) {
+        NSError *error = nil;
+        reject(@"no_events", @"Must call init function first", error);
+    } else {
+        [[PrinterSDK defaultPrinterSDK] scanPrintersWithCompletion:^(Printer* printer){
+            [_printerArray addObject:printer];
+            NSMutableArray *mapped = [NSMutableArray arrayWithCapacity:[_printerArray count]];
+            [_printerArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+                NSDictionary *dict = @{ @"device_name" : printer.name, @"inner_mac_address" : printer.UUIDString};
+                [mapped addObject:dict];
+            }];
+            resolve(@[mapped]);
+        }];
+    }
 }
 
-RCT_EXPORT_METHOD(connectPrinter:(NSString *)innerAddress
-                  connect_printer_resolver:(RCTPromiseResolveBlock)resolve
-                  connect_printer_rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(connectPrinter:(NSString *)inner_mac_address
+                 connect_printer_resolver:(RCTPromiseResolveBlock)resolve
+                 connect_printer_rejecter:(RCTPromiseRejectBlock)reject) {
     // TODO
     resolve(@"TODO");
 }
 
-RCT_EXPORT_METHOD(printRawData:(NSString *)base64
-                  print_data_resolver:(RCTPromiseResolveBlock)resolve
-                  print_data_rejecter:(RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(printRawData:(NSString *)text
+                 print_data_resolver:(RCTPromiseResolveBlock)resolve
+                 print_data_rejecter:(RCTPromiseRejectBlock)reject) {
     // TODO
     resolve(@"TODO");
 }
@@ -57,7 +63,7 @@ RCT_REMAP_METHOD(closeConn,
                  close_connect_resolver:(RCTPromiseResolveBlock)resolve
                  close_connect_rejecter:(RCTPromiseRejectBlock)reject) {
     [[PrinterSDK defaultPrinterSDK] disconnect];
-    resolve(@"Successful");
+    resolve(@"Successful disconnect");
 }
 
 @end
