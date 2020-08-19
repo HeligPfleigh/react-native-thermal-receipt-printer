6d743f069f6398ced5180c3cf1f4f49f298610e4
goldenhands92 <tuan.tran@mttjsc.com>
2020-08-19 19:09:30 +0700

    chore(example): add example for printing in usb and ble printer

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,119 @@
 import * as React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import * as RNThermalReceiptPrinter from "react-native-thermal-receipt-printer";
+import { StyleSheet, View, Text, Button, Picker } from "react-native";
+import {
+  BLEPrinter,
+  NetPrinter,
+  USBPrinter,
+  IUSBPrinter,
+  IBLEPrinter,
+  INetPrinter,
+} from "react-native-thermal-receipt-printer";
+import Loader from "./Loader";
+
+const printerList = {
+  ble: BLEPrinter,
+  net: NetPrinter,
+  usb: USBPrinter,
+};
 
 export default function App() {
+  const [selectedValue, setSelectedValue] = React.useState<
+    keyof typeof printerList
+  >("ble");
+  const [devices, setDevices] = React.useState([]);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [selectedPrinter, setSelectedPrinter] = React.useState<
+    IUSBPrinter | IBLEPrinter | INetPrinter
+  >({});
+
   React.useEffect(() => {
-    console.log(RNThermalReceiptPrinter);
-  }, []);
+    const init = async () => {
+      const Printer = printerList[selectedValue];
+      try {
+        setLoading(true);
+        await Printer.init();
+        const results = await Printer.getDeviceList();
+        setDevices(
+          results.map((item) => ({ ...item, printerType: selectedValue }))
+        );
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    init();
+  }, [selectedValue]);
+
+  React.useEffect(() => {
+    const connect = async () => {
+      try {
+        setLoading(true);
+        switch (selectedPrinter.printerType) {
+          case "ble":
+            await BLEPrinter.connectPrinter(selectedPrinter.inner_mac_address);
+            break;
+          case "net":
+            // TODO
+            break;
+          case "usb":
+            await USBPrinter.connectPrinter(
+              selectedPrinter.vendorId,
+              selectedPrinter.productId
+            );
+            break;
+          default:
+        }
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    connect();
+  }, [selectedPrinter]);
+
+  const handlePrint = async () => {
+    try {
+      const Printer = printerList[selectedValue];
+      await Printer.printText("<C>sample text</C>\n");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Example</Text>
+      <Text>Select printer type: </Text>
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 50, width: 150 }}
+        onValueChange={setSelectedValue}
+      >
+        {Object.keys(printerList).map((item, index) => (
+          <Picker.Item
+            label={item.toUpperCase()}
+            value={item}
+            key={`printer-type-item-${index}`}
+          />
+        ))}
+      </Picker>
+      <Text>Select printer: </Text>
+      <Picker
+        selectedValue={selectedPrinter}
+        style={{ height: 50, width: 150 }}
+        onValueChange={setSelectedPrinter}
+      >
+        {devices.map((item, index) => (
+          <Picker.Item
+            label={item.device_name}
+            value={item}
+            key={`printer-item-${index}`}
+          />
+        ))}
+      </Picker>
+      <Button title="Print sample" onPress={handlePrint} />
+      <Loader loading={loading} />
     </View>
   );
 }
@@ -17,7 +121,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
+    padding: 16,
   },
 });
