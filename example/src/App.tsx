f709507075e687439658a95cc11ef70192544d0d
Tuan Tran <trantuan1892@gmail.com>
2021-04-07 21:48:44 +0700

    fix: port of net printer should be number

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -73,7 +73,7 @@ export default function App() {
           case "net":
             await NetPrinter.connectPrinter(
               selectedPrinter?.host || "",
-              selectedPrinter?.port || ""
+              selectedPrinter?.port || 9100
             );
             break;
           case "usb":
