638343b02e14559acfead450b43f003c053598ad
Ahmed Ajmal <muhammad.ajmal@arkhitech.com>
2021-11-15 15:59:53 +0500

    ios code for qrcode implementation

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -27,6 +27,7 @@ export declare const USBPrinter: {
     printText: (text: string, opts?: PrinterOptions) => void;
     printBill: (text: string, opts?: PrinterOptions) => void;
     printImage: (imgUrl: string, opts?: {}) => void;
+    printQrCode: (qrCode: string, opts?: {}) => void;
 };
 export declare const BLEPrinter: {
     init: () => Promise<void>;
@@ -35,6 +36,7 @@ export declare const BLEPrinter: {
     closeConn: () => Promise<void>;
     printText: (text: string, opts?: PrinterOptions) => void;
     printBill: (text: string, opts?: PrinterOptions) => void;
+    printQrCode: (qrCode: string, opts?: {}) => void;
 };
 export declare const NetPrinter: {
     init: () => Promise<void>;
@@ -44,6 +46,7 @@ export declare const NetPrinter: {
     printText: (text: string, opts?: {}) => void;
     printBill: (text: string, opts?: {}) => void;
     printImage: (imgUrl: string, opts?: {}) => void;
+    printQrCode: (qrCode: string, opts?: {}) => void;
 };
 export declare const NetPrinterEventEmitter: NativeEventEmitter;
 export declare enum RN_THERMAL_RECEIPT_PRINTER_EVENTS {
